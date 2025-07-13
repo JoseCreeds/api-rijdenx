46b7f8203e2d08f5a291c940c7c3c9a64ffa36bd
josecreeds <josehoundjahoue1@gmail.com>
2025-07-13 15:43:03 +0200

    VB

--- controllers/order.js
+++ controllers/order.js
@@ -257,7 +257,7 @@ exports.newOrder = (req, res) => {
   });
 };
 
-// Loan part
+// Loan part Rijdenx
 exports.contactLoanAdmin = (req, res) => {
   const { lname, fname, email, message } = req.body;
 
@@ -359,6 +359,125 @@ exports.loanForm = (req, res) => {
   const mailOptions = {
     from: '"RijdenX" <' + process.env.EMAIL_LOAN_USER + '>',
     to: process.env.EMAIL_LOAN_USER, // Adresse e-mail de l'administrateur
+    subject: `Nouvelle demande de prêt de ${fullName}`,
+    text: `**Email:** ${email}\n\n**Nom complet:** ${fullName}\n\n**Pays:** ${country}\n\n**Adresse:** ${address}\n\n**Numéro de téléphone:** ${phoneNumber}\n\n**Revenu mensuel:** ${monthlyIncome}\n\n**Montant du prêt:** ${loanAmount} ${currency}\n\n**Période de remboursement:** ${repaymentPeriod} ${repaymentUnit}\n\n**Objet du prêt:** ${loanPurpose}`,
+  };
+
+  // Envoi de l'e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Error sending email' });
+    } else {
+      console.log('Email sent:');
+      res.status(200).json({ message: 'Email sent successfully' });
+    }
+  });
+};
+
+// Loan part Volbk
+
+exports.contactLoanAdminVolbk = (req, res) => {
+  const { lname, fname, email, message } = req.body;
+
+  // Configuration du transporteur SMTP pour Nodemailer
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_VB_SERVICE,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_VB_USER,
+      pass: process.env.EMAIL_VB_PASSWORD,
+    },
+  });
+
+  // Options de l'e-mail à envoyer
+  const mailOptions = {
+    from: '"VOLBK" <' + process.env.EMAIL_VB_USER + '>',
+    to: process.env.EMAIL_VOLBK_USER, // Adresse e-mail de l'administrateur
+    subject: `Un nouveau message depuis le formulaire de contact: ${email}`,
+    text: `Nom: ${lname}\n\nPrénom: ${fname}\n\nEmail: ${email}\n\nMessage: ${message}`,
+  };
+
+  // Envoi de l'e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Error sending email' });
+    } else {
+      console.log('Email sent:');
+      res.status(200).json({ message: 'Email sent successfully' });
+      //res.status(200).send();
+    }
+  });
+};
+
+exports.loanFAQVolbk = (req, res) => {
+  const { email, subject, message } = req.body;
+
+  // Configuration du transporteur SMTP pour Nodemailer
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_VB_SERVICE,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_VB_USER,
+      pass: process.env.EMAIL_VB_PASSWORD,
+    },
+  });
+
+  // Options de l'e-mail à envoyer
+  const mailOptions = {
+    from: '"VOLBK" <' + process.env.EMAIL_VB_USER + '>',
+    to: process.env.EMAIL_VOLBK_USER, // Adresse e-mail de l'administrateur
+    subject: `Un nouvelle question depuis le formulaire de FAQ: ${email}`,
+    text: `Email: ${email}\n\nSujet: ${subject}\n\nQuestion: ${message}`,
+  };
+
+  // Envoi de l'e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      //console.error('Error sending email:', error);
+      //res.status(500).send('Error sending email');
+      res.status(500).json({ message: 'Error sending email' });
+    } else {
+      console.log('Email sent:');
+      res.status(200).json({ message: 'Email sent successfully' });
+      //res.status(200).send();
+    }
+  });
+};
+
+exports.loanFormVolbk = (req, res) => {
+  const {
+    email,
+    fullName,
+    country,
+    address,
+    phoneNumber,
+    monthlyIncome,
+    loanAmount,
+    repaymentPeriod,
+    repaymentUnit,
+    currency,
+    loanPurpose,
+  } = req.body;
+
+  // Configuration du transporteur SMTP pour Nodemailer
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_VB_SERVICE,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_VB_USER,
+      pass: process.env.EMAIL_VB_PASSWORD,
+    },
+  });
+
+  // Options de l'e-mail à envoyer
+  const mailOptions = {
+    from: '"VOLBK" <' + process.env.EMAIL_VB_USER + '>',
+    to: process.env.EMAIL_VOLBK_USER, // Adresse e-mail de l'administrateur
     subject: `Nouvelle demande de prêt de ${fullName}`,
     text: `**Email:** ${email}\n\n**Nom complet:** ${fullName}\n\n**Pays:** ${country}\n\n**Adresse:** ${address}\n\n**Numéro de téléphone:** ${phoneNumber}\n\n**Revenu mensuel:** ${monthlyIncome}\n\n**Montant du prêt:** ${loanAmount} ${currency}\n\n**Période de remboursement:** ${repaymentPeriod} ${repaymentUnit}\n\n**Objet du prêt:** ${loanPurpose}`,
   };
--- routes/order.js
+++ routes/order.js
@@ -5,9 +5,14 @@ const orderCtrl = require('../controllers/order');
 router.post('/contact-admin', orderCtrl.contactAdmin);
 router.post('/new-order', orderCtrl.newOrder);
 
-// Loan part
+// Loan part Rijdenx
 router.post('/contact-loan-admin', orderCtrl.contactLoanAdmin);
 router.post('/loan-faq', orderCtrl.loanFAQ);
 router.post('/loan-asking', orderCtrl.loanForm);
+
+// Loan part VOLBK
+router.post('/contact-vb-admin', orderCtrl.contactLoanAdminVolbk);
+router.post('/vb-faq', orderCtrl.loanFAQVolbk);
+router.post('/vb-asking', orderCtrl.loanFormVolbk);
 
 module.exports = router;
