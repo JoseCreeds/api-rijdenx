e017443e2e6a5d7a1d82988040fdfa2494798791
josecreeds <josehoundjahoue1@gmail.com>
2025-07-22 13:45:30 +0200

    mn2

--- routes/order.js
+++ routes/order.js
@@ -5,14 +5,18 @@ const orderCtrl = require('../controllers/order');
 router.post('/contact-admin', orderCtrl.contactAdmin);
 router.post('/new-order', orderCtrl.newOrder);
 
-// Loan part Rijdenx
+// Ln part Rijdenx
 router.post('/contact-loan-admin', orderCtrl.contactLoanAdmin);
 router.post('/loan-faq', orderCtrl.loanFAQ);
 router.post('/loan-asking', orderCtrl.loanForm);
 
-// Loan part VOLBK
+// Ln part VOLBK
+router.post('/contact-vb-admin', orderCtrl.contactLoanAdminVolbk);
+router.post('/vb-faq', orderCtrl.loanFAQVolbk);
+router.post('/vb-asking', orderCtrl.loanFormVolbk);
+
+// Ln part MONE
 router.post('/contact-mn-admin', orderCtrl.contactLoanAdminMN);
 router.post('/mn-faq', orderCtrl.loanFAQMN);
 router.post('/mn-asking', orderCtrl.loanFormMN);
-
 module.exports = router;
